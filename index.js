8953f31a139945b9f6235de636b8e3063546295f
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2020-12-06 15:58:05 +0200

    Відкриття модального вікна при натисканні на елементі галереї.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import images from "./gallery-items.js";
 
 let imageList = document.querySelector(".js-gallery");
+let openModal = document.querySelector(".lightbox");
+let closeModal = document.querySelector("[data-action]");
+let modalImg = document.querySelector(".lightbox__image");
 
 images.forEach(({ preview, original, description }) => {
   let elem = document.createElement("li");
@@ -18,3 +21,25 @@ images.forEach(({ preview, original, description }) => {
   elem.append(link);
   imageList.appendChild(elem);
 });
+
+function onOpenModal(e) {
+  e.preventDefault();
+  let targetItems = document.querySelectorAll(".gallery__image");
+
+  for (let targetItem of targetItems) {
+    if (e.target === targetItem) {
+      openModal.classList.add("is-open");
+      let currentImageLink = e.target.dataset.source;
+      console.log("object :>> ", currentImageLink);
+      modalImg.setAttribute("src", currentImageLink);
+    }
+  }
+}
+
+function onCloseModal() {
+  openModal.classList.remove("is-open");
+  modalImg.setAttribute("src", "");
+}
+
+imageList.addEventListener("click", onOpenModal);
+closeModal.addEventListener("click", onCloseModal);
