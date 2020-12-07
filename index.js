cfe43eb540adddd7496234cac7b265d0ca8d4216
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2020-12-07 10:51:47 +0200

    Закриття модального вікна після натискання клавіші ESC.

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ let closeModal = document.querySelector("[data-action]");
 let modalImg = document.querySelector(".lightbox__image");
 let modalBackdrop = document.querySelector(".lightbox__overlay");
 
-images.forEach(({ preview, original, description }) => {
+images.forEach(({ preview, original, description }, index) => {
   let elem = document.createElement("li");
   let link = document.createElement("a");
   let img = document.createElement("img");
 
   elem.classList.add("gallery__item");
+  elem.setAttribute("data-item", index);
   link.classList.add("gallery__link");
+  link.setAttribute("data-link", index);
   link.href = original;
   img.classList.add("gallery__image");
+  img.setAttribute("data-image", index);
   img.src = preview;
   img.setAttribute("data-source", original);
   img.alt = description;
@@ -28,25 +31,42 @@ function onOpenModal(e) {
 
   let targetItems = document.querySelectorAll(".gallery__image");
 
+  openModal.setAttribute("data-open", true);
+
+  console.log(" :>> ", openModal.dataset.open);
+
   for (let targetItem of targetItems) {
     if (e.target === targetItem) {
       openModal.classList.add("is-open");
       let currentImageLink = e.target.dataset.source;
-      console.log("object :>> ", currentImageLink);
+      //console.log("object :>> ", currentImageLink);
       modalImg.setAttribute("src", currentImageLink);
     }
   }
+
+  window.addEventListener("keydown", onPressEscapeButtonClose);
+  window.addEventListener("keydown", onArrowButtonsClick);
 }
 
 function onCloseModal() {
   openModal.classList.remove("is-open");
   modalImg.setAttribute("src", "");
+  openModal.setAttribute("data-open", false);
+  console.log(" :>> ", openModal.dataset.open);
+
+  window.removeEventListener("keydown", onPressEscapeButtonClose);
+  window.removeEventListener("keydown", onArrowButtonsClick);
 }
 
 function onBackdropClicClose(e) {
   if (e.target === modalBackdrop) {
     openModal.classList.remove("is-open");
     modalImg.setAttribute("src", "");
+    openModal.setAttribute("data-open", false);
+    console.log(" :>> ", openModal.dataset.open);
+
+    window.removeEventListener("keydown", onPressEscapeButtonClose);
+    window.removeEventListener("keydown", onArrowButtonsClick);
   }
 }
 
@@ -54,10 +74,18 @@ function onPressEscapeButtonClose(e) {
   if (e.keyCode === 27) {
     openModal.classList.remove("is-open");
     modalImg.setAttribute("src", "");
+    openModal.setAttribute("data-open", false);
+    console.log(" :>> ", openModal.dataset.open);
+
+    window.removeEventListener("keydown", onPressEscapeButtonClose);
+    window.removeEventListener("keydown", onArrowButtonsClick);
   }
+}
+
+function onArrowButtonsClick(e) {
+  console.log();
 }
 
 imageList.addEventListener("click", onOpenModal);
 closeModal.addEventListener("click", onCloseModal);
 modalBackdrop.addEventListener("click", onBackdropClicClose);
-window.addEventListener("keydown", onPressEscapeButtonClose);
